66b940fa53b62ff5ebcfee3e161f5eebef39d7ab
Aibek <nomad.aibek@gmail.com>
2020-08-09 20:42:45 +0600

    created admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from webapp.models import Issue, Type, Status
 
-# Register your models here.
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title')
+    list_display_links = ('pk', 'title')
+    search_fields = ('title',)
+
+
+class IssueStatusAdmin(admin.ModelAdmin):
+    list_display = ('status',)
+
+
+class IssueTypesAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Status, IssueStatusAdmin)
+admin.site.register(Type, IssueTypesAdmin)
