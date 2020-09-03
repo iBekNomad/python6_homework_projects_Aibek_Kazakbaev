06b6cdd515aad6153b6a9e5ac5685afa0966a9b4
Aibek <nomad.aibek@gmail.com>
2020-09-03 21:31:04 +0600

    Добавлена регистрация

---
+++ source/accounts/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth.models import User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email']
+        fields_classes = {'username': UsernameField}
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,21 +1,45 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+
+from accounts.forms import MyUserCreationForm
+from django.views.generic import CreateView
 
 
-def login_view(request):
-    context = {}
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('index')
-        else:
-            context['has_error'] = True
-    return render(request, 'registration/login.html', context=context)
+# def login_view(request):
+#     context = {}
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('index')
+#         else:
+#             context['has_error'] = True
+#     return render(request, 'registration/login.html', context=context)
+#
+#
+# def logout_view(request):
+#     logout(request)
+#     return redirect('index')
 
 
-def logout_view(request):
-    logout(request)
-    return redirect('index')
+class RegisterView(CreateView):
+    model = User
+    template_name = 'user_create.html'
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next:
+            next_url = self.request.POST.get('next')
+        if not next:
+            next_url = reverse('index')
+        return next_url
