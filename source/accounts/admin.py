ad632e63fd6a51b07ef6eea4cc3408caabcb3895
Aibek <nomad.aibek@gmail.com>
2020-09-11 12:19:51 +0600

    Профиль админа зарегистрирован в админке

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import AuthToken, Profile
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(AuthToken)
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    exclude = []
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInLine]
+
+
+User = get_user_model()
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
